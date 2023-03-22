7f2262f59771a5cc39f7b66e1f3d203088c61280
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-03-22 06:44:34 -0500

    update

--- src/App.js
+++ src/App.js
@@ -11,8 +11,11 @@ const App = () => {
   const startListening = () => {
     SpeechRecognition.startListening({ continuous: true, language: 'es-AR' });
   }
+  let interim = "";
   let palabras = interimTranscript.split(" ");
-  let count = (transcript.match(/boludo/g) || []).length;
+  let count1 = (transcript.match(/boludo/g) || []).length;
+  let count2 = (transcript.match(/boluda/g) || []).length;
+  let count = count1 + count2;
   console.log("boludos-transcript-"+count);
   
   let audio = document.getElementById("audio");
@@ -24,18 +27,24 @@ const App = () => {
     }
   }
 
+  if (interimTranscript !== "") {
+    interim = "Interim: " + interimTranscript;
+  } else {
+    interim = "";
+  }
+
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
   return (
-    <div>
-      <p>Microphone: {listening ? 'on' : 'off'}</p>
+    <div class="my-5">
+      <p class="mb-2">Microphone: {listening ? 'on' : 'off'}</p>
       <button onClick={startListening}>Start</button>
       <button onClick={SpeechRecognition.stopListening}>Stop</button>
       <button onClick={resetTranscript}>Reset</button>
-      <p>{transcript}</p>
-      <p>{interimTranscript}</p>
-      <p>Contador de boludos: {count}</p>
+      <p class="my-2">Contador: {count}</p>
+      <div id="transcript" class="m-3 p-4">{transcript}</div>
+      <p id="interim" class="mx-3"><i>{interim}</i></p>
       <audio id="audio" controls>
         <source type="audio/mp3" src="/dijiste-boludo.mp3"></source>
       </audio>
